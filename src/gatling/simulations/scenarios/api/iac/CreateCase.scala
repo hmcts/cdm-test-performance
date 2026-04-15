2bd9ef377a96d2b002383a3a629301b9d582cd1c
jmcadam <mcadam88@gmail.com>
2026-04-15 16:15:34 +0100

    add probate code to branch

--- src/gatling/simulations/scenarios/api/iac/CreateCase.scala
+++ src/gatling/simulations/scenarios/api/iac/CreateCase.scala
@@ -16,17 +16,14 @@ object CreateCase {
     rnd.alphanumeric.filter(_.isLetter).take(length).mkString
   }
 
-  //Used to override the microservice for this case type set within the common-performance repo
-  val IA_Asylum = CcdCaseTypes.IA_Asylum.copy(microservice = "iac")
-
   val execute =
 
     exec(_.setAll("randomString" -> randomString(5),
       "dob" -> DateUtils.getDatePastRandom("yyyy-MM-dd", minYears = 20, maxYears = 50),
       "todayDate" -> DateUtils.getDateNow("yyyy-MM-dd")))
 
-      .feed(feedIACUserData)
-      .exec(CcdHelper.uploadDocumentToCdam("#{email}", "#{password}", IA_Asylum, "1MB.pdf"))
-      .exec(CcdHelper.createCase("#{email}", "#{password}", CcdCaseTypes.IA_Asylum, "startAppeal", "bodies/iac/IACCreateCase.json"))
-      .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Asylum, "#{caseId}", "submitAppeal", "bodies/iac/IACSubmitAppeal.json"))
+    .feed(feedIACUserData)
+    .exec(CcdHelper.uploadDocumentToCdam("#{email}", "#{password}", CcdCaseTypes.IA_Asylum.copy(microservice = "xui_webapp"), "1MB.pdf"))
+    .exec(CcdHelper.createCase("#{email}", "#{password}", CcdCaseTypes.IA_Asylum, "startAppeal", "bodies/iac/IACCreateCase.json"))
+    .exec(CcdHelper.addCaseEvent("#{email}", "#{password}", CcdCaseTypes.IA_Asylum, "#{caseId}", "submitAppeal", "bodies/iac/IACSubmitAppeal.json"))
 }
